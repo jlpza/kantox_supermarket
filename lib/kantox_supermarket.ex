f3c5782246e8e706eb8dd4e6a8e7ee65a09fadea
Jorge Lopez <jorlopezalonso@gmail.com>
2023-06-27 23:00:44 +0200

    Initial tests and initial version of the code

--- lib/kantox_supermarket.ex
+++ lib/kantox_supermarket.ex
@@ -3,16 +3,19 @@ defmodule KantoxSupermarket do
   Documentation for `KantoxSupermarket`.
   """
 
-  @doc """
-  Hello world.
+  def calculate_price(basket) do
+    basket_frequencies = Enum.frequencies(basket)
+    gr1 = Map.get(basket_frequencies, "GR1", 0)
+    sr1 = Map.get(basket_frequencies, "SR1", 0)
+    cf1 = Map.get(basket_frequencies, "CF1", 0)
 
-  ## Examples
+    # Green Tea, do one get one free
+    gr1_price = (gr1 - div(gr1, 2)) * 3.11
+    # Strawberries discount if you buy 3 or more
+    sr1_price = if sr1 >= 3, do: sr1 * 4.50, else: sr1 * 5.00
+    # Coffee discount if you buy 3 or more
+    cf1_price = if cf1 >= 3, do: cf1 * 11.23 * (2 / 3), else: cf1 * 11.23
 
-      iex> KantoxSupermarket.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    Float.ceil(gr1_price + sr1_price + cf1_price, 2)
   end
 end
--- test/kantox_supermarket_test.exs
+++ test/kantox_supermarket_test.exs
@@ -3,6 +3,9 @@ defmodule KantoxSupermarketTest do
   doctest KantoxSupermarket
 
   test "greets the world" do
-    assert KantoxSupermarket.hello() == :world
+    assert 22.45 == KantoxSupermarket.calculate_price(["GR1", "SR1", "GR1", "GR1", "CF1"])
+    assert 3.11 == KantoxSupermarket.calculate_price(["GR1", "GR1"])
+    assert 16.61 == KantoxSupermarket.calculate_price(["SR1", "SR1", "GR1", "SR1"])
+    assert 30.57 == KantoxSupermarket.calculate_price(["GR1", "CF1", "SR1", "CF1", "CF1"])
   end
 end
