2ce59fcc40dd927506940f45cc15298cad861ef3
Jorge Lopez <jorlopezalonso@gmail.com>
2023-07-01 12:44:44 +0200

    some comments for readability

--- lib/discounts/buy_some_get_some_free.ex
+++ lib/discounts/buy_some_get_some_free.ex
@@ -22,9 +22,11 @@ defmodule Discounts.BuySomeGetSomeFree do
       {products, other_products} = Enum.split_with(shopping_cart, &(&1.id == product_id))
 
       products
+      # Split 'products' into batches of 'needed_products + free_products' size.
       |> Enum.chunk_every(needed_products + free_products)
-      |> Enum.flat_map(fn products ->
-        {paid_products, free_products} = Enum.split(products, needed_products)
+      |> Enum.flat_map(fn product_batch ->
+        # The first 'needed_products' products will be paid, while the remaining 'free_products' products will be free.
+        {paid_products, free_products} = Enum.split(product_batch, needed_products)
         paid_products ++ Enum.map(free_products, &%Product{&1 | price: 0.00})
       end)
       |> Kernel.++(other_products)
