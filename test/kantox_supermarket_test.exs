6e890035c4e334897c07cc3742af74d350e4dcc1
Jorge Lopez <jorlopezalonso@gmail.com>
2023-07-01 14:16:47 +0200

    Added mixed tests

--- test/kantox_supermarket_test.exs
+++ test/kantox_supermarket_test.exs
@@ -1,16 +1,59 @@
 defmodule KantoxSupermarketTest do
   use ExUnit.Case
+  import Mock
 
-  doctest KantoxSupermarket
+  alias Discounts.{BuySomeGetSomeFree, DiscountForBuyingMoreThan, NewPriceForBuyingMoreThan}
 
   setup_all _ do
     Data.load_products("data/products_test.csv")
   end
 
-  test "Standard scenarios" do
+  test "Predefined scenarios of the exercise" do
     assert 22.45 == KantoxSupermarket.calculate_price(["GR1", "SR1", "GR1", "GR1", "CF1"])
     assert 3.11 == KantoxSupermarket.calculate_price(["GR1", "GR1"])
     assert 16.61 == KantoxSupermarket.calculate_price(["SR1", "SR1", "GR1", "SR1"])
     assert 30.57 == KantoxSupermarket.calculate_price(["GR1", "CF1", "SR1", "CF1", "CF1"])
+  end
+
+  test_with_mock "Mixed products", Discounts,
+    get_discounts: fn ->
+      [
+        %BuySomeGetSomeFree{product_id: "GR1", needed_products: 1, free_products: 1},
+        %NewPriceForBuyingMoreThan{product_id: "SR1", minimum_units: 3, new_price: 4.50},
+        %DiscountForBuyingMoreThan{product_id: "CF1", minimum_units: 3, discount_rate: 2 / 3}
+      ]
+    end do
+    # One discount at a time
+    assert 35.57 ==
+             KantoxSupermarket.calculate_price(["GR1", "SR1", "CF1", "GR1", "SR1", "CF1"])
+
+    assert 39.07 ==
+             KantoxSupermarket.calculate_price(["SR1", "GR1", "SR1", "CF1", "SR1", "CF1"])
+
+    assert 35.57 ==
+             KantoxSupermarket.calculate_price(["CF1", "GR1", "CF1", "SR1", "CF1", "SR1"])
+
+    # Two discounts at the same time
+    assert 39.07 ==
+             KantoxSupermarket.calculate_price(["GR1", "SR1", "CF1", "SR1", "CF1", "SR1", "CF1"])
+
+    assert 39.07 ==
+             KantoxSupermarket.calculate_price(["SR1", "GR1", "SR1", "CF1", "SR1", "CF1", "GR1"])
+
+    assert 35.57 ==
+             KantoxSupermarket.calculate_price(["CF1", "GR1", "CF1", "SR1", "CF1", "SR1", "GR1"])
+
+    # All discounts at the same time
+    assert 39.07 ==
+             KantoxSupermarket.calculate_price([
+               "GR1",
+               "SR1",
+               "CF1",
+               "SR1",
+               "CF1",
+               "SR1",
+               "CF1",
+               "GR1"
+             ])
   end
 end
